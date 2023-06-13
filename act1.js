1ef55a3b4db39510b35b3f847a2f407ea78efe73
OriDaer <d47709740@alumnos.itr.edu.ar>
2023-06-13 11:36:19 -0300

    sumPrimos finalizado-condicion:num>1 y n=2

--- act1.js
+++ act1.js
@@ -1,14 +1,13 @@
-//¿PORQUE NO TOMA LA POSICION 00 Y DE UNA PASA A VALER 11?
-//sumo todos lo elementos de la matriz
+//sumo todos lo elementos de la matriz solo si son num primos
 function PrimeSum(matrix){
     let res=0;
     for(let col=0;col< matrix.length;col++){
         for(let fil=0;fil<matrix.length;fil++){
-          //nota:la idea es q no tome al 1 cm valor de num primos  
-            if( matrix[col] [fil]>1 && matrix[col][fil]% 2 ===1){//determino si son primos o no
+          //nota:no tome al 1 cm valor de num primos pero al 2 si 
+            if( matrix[col] [fil]>1 && matrix[col][fil]% 2 ===1 || matrix[col][fil]===2){//determino si son primos o no
                 //usamos template literal para ver q valores esta teniendo
                 console.log(`Estoy acumulando valor col:${col} fil:${fil}`);
-                console.log(`valgo en col:${matrix[col].length} y en fil:${matrix[fil].length}`);
+                console.log(`valgo en col:${matrix[col]} y en fil:${matrix[fil]}`);
             res+=matrix[col][fil];
             }
         }
